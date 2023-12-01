2487be8e606426a34b14e5f230707ce8b53c0eaf
koznrokhan <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>
2023-12-02 01:02:10 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+const PORT = 5001;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/set', (req, res) => {
+    res.cookie('name', 'nodejs');
+    return res.status(200).end();
+});
+
+app.get('/get', (req, res) => {
+    const cookies = req.cookies;
+    return res.status(200).json({ cookies });
+});
+
+app.listen(PORT, () => {
+    console.log(PORT, '포트로 서버가 열렸습니다.');
+});
